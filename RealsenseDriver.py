a4dc3e39bc3d93dbe5cc688614a712c5bc937027
Josh McCulloch <mcculloch@windowslive.com>
2020-01-24 13:36:53 +1300

    rendering disparity map between ir cameras

--- RealsenseDriver.py
+++ RealsenseDriver.py
@@ -7,13 +7,14 @@ import time
 
 class RealsenseDriver(object):
 	
-	def __init__(self, color_path, ir1_path, ir2_path, depth_path):
+	def __init__(self, color_path=None, ir1_path=None, ir2_path=None, depth_path=None):
 		self.pipeline = rs.pipeline()
 		self.config = rs.config()
 		self.color_path = color_path
 		self.ir1_path = ir1_path
 		self.ir2_path = ir2_path
 		self.depth_path = depth_path
+		self.threaded_save = True
 		
 		# Enable color stream at full res
 		self.config.enable_stream(
@@ -74,6 +75,11 @@ class RealsenseDriver(object):
 		print("Done")
 		
 		
+	def set_laser(self, value):
+		for s in self.sensors:
+			if s.is_depth_sensor():
+				s.set_option(rs.option.emitter_enabled, value)
+				
 	def warm_camera(self, sec=1.0):
 		"""
 		Run camera for (sec) seconds to allow auto exposure to settle.
@@ -92,29 +98,51 @@ class RealsenseDriver(object):
 		print(intrinsics.ppy)
 		print(intrinsics.width)
 		print(intrinsics.coeffs)
-			
-	def save(self, filename):
+		
+	def camera_matrix(self, intrinsics):
+		return np.array([
+		[intrinsics.fx, 0, intrinsics.ppx],
+		[0, intrinsics.fy, intrinsics.ppy],
+		[0,0,1]]), np.array(intrinsics.coeffs), (intrinsics.width, intrinsics.height)
+		
+	def ir1_camera_matrix(self):
+		intrinsics = self.ir1_profile.get_intrinsics()
+		return self.camera_matrix(intrinsics)
+		
+	def ir2_camera_matrix(self):
+		intrinsics = self.ir2_profile.get_intrinsics()
+		return self.camera_matrix(intrinsics)
+		
+	def l2r_extrinsics(self):
+		return self.ir1_profile.get_extrinsics_to(self.ir2_profile)
+		
+	def get(self):
 		frames = self.pipeline.wait_for_frames()
 		
 		color_frame = frames.get_color_frame()
 		color_image = np.asanyarray(color_frame.get_data())
-		#Image.fromarray(color_image).save(join(self.color_path, filename+".png"))
-		threading.Thread(target=lambda:Image.fromarray(color_image).save(join(self.color_path, filename+".jpg"), 'JPEG', quality=95)).start()
 		
 		ir1_frame = frames.get_infrared_frame(1)
 		ir1_image = np.asanyarray(ir1_frame.get_data())
-		#Image.fromarray(ir1_image).save(join(self.ir1_path, filename+".png"))
-		threading.Thread(target=lambda:Image.fromarray(ir1_image).save(join(self.ir1_path, filename+".jpg"), 'JPEG', quality=95)).start()
-
 		
 		ir2_frame = frames.get_infrared_frame(2)
 		ir2_image = np.asanyarray(ir2_frame.get_data())
-		#Image.fromarray(ir2_image).save(join(self.ir2_path, filename+".png"))
-		threading.Thread(target=lambda:Image.fromarray(ir2_image).save(join(self.ir2_path, filename+".jpg"), 'JPEG', quality=95)).start()
 		
 		depth_frame = frames.get_depth_frame()
 		depth_image = np.asanyarray(depth_frame.get_data())
-		#Image.fromarray(depth_image).save(join(self.depth_path, filename+".png"))
-		threading.Thread(target=lambda:Image.fromarray(depth_image).save(join(self.depth_path, filename+".png"))).start()
 		
-		return Image.fromarray(ir2_image)
+		return color_image, ir1_image, ir2_image, depth_image
+		
+			
+	def save(self, filename):
+		color_image, ir1_image, ir2_image, depth_image = self.get()
+		if self.threaded_save:
+			threading.Thread(target=lambda:Image.fromarray(color_image).save(join(self.color_path, filename+".jpg"), 'JPEG', quality=95)).start()
+			threading.Thread(target=lambda:Image.fromarray(ir1_image).save(join(self.ir1_path, filename+".jpg"), 'JPEG', quality=95)).start()
+			threading.Thread(target=lambda:Image.fromarray(ir2_image).save(join(self.ir2_path, filename+".jpg"), 'JPEG', quality=95)).start()
+			threading.Thread(target=lambda:Image.fromarray(depth_image).save(join(self.depth_path, filename+".png"))).start()
+		else:
+			Image.fromarray(color_image).save(join(self.color_path, filename+".png"))
+			Image.fromarray(ir1_image).save(join(self.ir1_path, filename+".png"))
+			Image.fromarray(ir2_image).save(join(self.ir2_path, filename+".png"))
+			Image.fromarray(depth_image).save(join(self.depth_path, filename+".png"))
--- RectificationTest.py
+++ RectificationTest.py
@@ -4,38 +4,100 @@ import pyrealsense2 as rs
 import time
 from os.path import join, isdir
 from os import mkdir
+import cv2
+import numpy as np
+
+def scale(image, scale):
+	return cv2.resize(image, (image.shape[1]//scale, image.shape[0]//scale))
+
+class Viewer(object):
+
+	def __init__(self):
+		cv2.namedWindow("Realsense", cv2.WINDOW_AUTOSIZE );
+
+	def update(self,image):
+		cv2.imshow("Realsense", image)
 
 from RealsenseDriver import RealsenseDriver
 
 if __name__ == "__main__":
-	base_dir = "./scan_%d" %time.time()
-	color_dir = join(base_dir, "color")
-	ir1_dir = join(base_dir, "ir1")
-	ir2_dir = join(base_dir, "ir2")
-	depth_dir = join(base_dir, "depth")
+	rsd = RealsenseDriver()
+	print(rsd.ir1_camera_matrix())
+	ir1_mat, ir1_coeffs, ir1_size = rsd.ir1_camera_matrix()
+	ir2_mat, ir2_coeffs, ir2_size = rsd.ir2_camera_matrix()
+	extrinsics = rsd.l2r_extrinsics()
+	
+	R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(
+		ir1_mat,
+		ir1_coeffs,
+		ir2_mat,
+		ir2_coeffs,
+		ir1_size,
+		np.array(extrinsics.rotation).reshape(3,3),
+		np.array(extrinsics.translation),
+		flags=0)
+		
+	m1x, m1y = cv2.initUndistortRectifyMap( ir1_mat, ir1_coeffs, R1, P1, ir1_size, cv2.CV_32FC1)
+	m2x, m2y = cv2.initUndistortRectifyMap( ir2_mat, ir2_coeffs, R2, P2, ir2_size, cv2.CV_32FC1)
+		
+	v = Viewer()
 	
 	
-	if not isdir(base_dir):
-		mkdir(base_dir)
+	laser_on = True
+	block_size = 5
+	rsd.set_laser(1)
+	while True:
+		key = cv2.waitKey(1)
+		if key == 108: # letter l
+			if laser_on:
+				laser_on = False
+				rsd.set_laser(0)
+			else:
+				laser_on = True
+				rsd.set_laser(1)
+			print("laser")
 		
-	if not isdir(color_dir):
-		mkdir(color_dir)
+		elif key == 82: # up arrow
+			block_size += 2
+			print("block size", block_size)
+			
+		elif key == 84: # down arrow
+			if block_size > 5:
+				block_size -= 2
+			print("block size", block_size)
+			
+		elif key > 0:
+			print(key)
+			break
+		color_image, ir1_image, ir2_image, depth_image = rsd.get()
 		
-	if not isdir(ir1_dir):
-		mkdir(ir1_dir)
+		ir1_image_sr = cv2.remap(ir1_image, m1x, m1y, cv2.INTER_CUBIC)
+		ir2_image_sr = cv2.remap(ir2_image, m2x, m2y, cv2.INTER_CUBIC)
 		
-	if not isdir(ir2_dir):
-		mkdir(ir2_dir)
+		#ir1_image_sr = cv2.absdiff(ir1_image_sr,ir2_image_sr)
 		
-	if not isdir(depth_dir):
-		mkdir(depth_dir)
+		#stereo_matcher = cv2.StereoSGBM_create(0, 80, 7)
+		stereo_matcher = cv2.StereoBM_create(80, block_size)
+		disparity = stereo_matcher.compute(ir1_image_sr,ir2_image_sr)
+		'''
+		print(np.min(disparity), np.max(disparity))
+		rtv, binary = cv2.threshold(disparity,2,255, cv2.THRESH_BINARY)
+		print(binary)
 		
-	rsd = RealsenseDriver(color_dir, ir1_dir, ir2_dir, depth_dir)
-	
+		rtv, labels, stats, centroids = cv2.connectedComponentsWithStats(np.uint8(binary),4)
+		print(labels.dtype)'''
+		disparity = np.uint8(disparity/16)
+		rtv, disparity = cv2.threshold(disparity, 30, 0, cv2.THRESH_TOZERO)
+		rtv, disparity = cv2.threshold(disparity, 200, 0, cv2.THRESH_TOZERO_INV)
+		
+		disparity = cv2.applyColorMap(np.uint8(disparity), cv2.COLORMAP_HOT);
+		
 
-	for i in range(1):
-		rsd.save("%d" %time.time())
-		time.sleep(1)
+		v.update(disparity)
+
+
+
+
 
 
 
